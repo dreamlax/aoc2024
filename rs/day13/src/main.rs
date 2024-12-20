61dde9d2c6a360ba74422d4e8c8619ddfcf8d918
Dave Lemonby <dave@lemonby.com>
2024-12-20 14:51:28 +1100

    Finished Rust day 13

--- rs/day13/src/main.rs
+++ rs/day13/src/main.rs
@@ -1,31 +1,30 @@
 use utils::timer::Timer;
-use std::collections;
-use std::error::Error;
 use std::fs::read_to_string;
 use std::path::PathBuf;
 use std::str::FromStr;
 
 #[derive(Debug)]
-struct Equation {
-    coeff_m: u32,
-    coeff_n: u32,
-    sum: u32
-}
-
-impl Equation {
-    fn find_intersection(&self, other: &Self) -> Option<(u32, u32)> {
-        None
-    }
-}
-
-#[derive(Debug)]
 struct ClawMachine {
-    equations: [Equation; 2]
+    m: [[i64; 2]; 2],
+    c: [i64; 2]
 }
 
 impl ClawMachine {
     fn count_tokens(&self) -> usize {
-        0
+        let determinant = self.m[0][0] * self.m[1][1] - self.m[0][1] * self.m[1][0];
+        let inverse = [[self.m[1][1], -self.m[0][1]], [-self.m[1][0], self.m[0][0]]];
+
+        let x = inverse[0][0] * self.c[0] + inverse[0][1] * self.c[1];
+        let y = inverse[1][0] * self.c[0] + inverse[1][1] * self.c[1];
+
+        let (x, x_rem) = (x / determinant, x % determinant);
+        let (y, y_rem) = (y / determinant, y % determinant);
+
+        if x_rem != 0 || y_rem != 0 || x < 0 || y < 0 {
+            return 0;
+        }
+        
+        x as usize * 3 + y as usize
     }
 }
 
@@ -43,14 +42,18 @@ impl FromStr for ClawMachine {
                 .filter_map(|(idx, s)| if idx == 1 || idx == 3 { Some(s) } else { None })
             )
             .map(str::parse)
-            .collect::<Result<Vec<u32>,_>>()
+            .collect::<Result<Vec<i64>,_>>()
             .map_err(|_| ClawMachineParseError)?;
 
+        let constant = if cfg!(feature = "part2") {
+            10_000_000_000_000
+        } else {
+            0
+        };
+
         Ok(Self {
-            equations: [
-                Equation { coeff_m: values[0], coeff_n: values[1], sum: values[4] },
-                Equation { coeff_m: values[2], coeff_n: values[3], sum: values[5] },
-            ]
+            m: [ [ values[0], values[2] ], [ values[1], values[3] ] ],
+            c: [ values[4] + constant, values[5] + constant ]
         })
     }
 }
@@ -63,12 +66,13 @@ fn main() {
         .expect("Should have file argument")
         .into();
 
-    let claw_machines = read_to_string(&path)
+    let total_tokens: usize = read_to_string(&path)
         .expect("Should be able to read input")
         .split("\n\n")
         .map(ClawMachine::from_str)
-        .collect::<Result<Vec<ClawMachine>,_>>()
-        .expect("Should be able to parse claw machines");
-
-    dbg!(claw_machines);
+        .map(Result::unwrap)
+        .map(|c| c.count_tokens())
+        .sum();
+        
+    println!("Answer: {total_tokens}");
 }
